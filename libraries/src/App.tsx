1bfcb8fbaa4b9d07208f3c1cf4b094e5a040eca3
pjh753156a <00-g-00@naver.com>
2024-04-18 17:57:55 +0900

    2024-04-18-61days

--- libraries/src/App.tsx
+++ libraries/src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import ZustandLibraty from 'src/zustand/ZustandLibraty';
 import { Route, Routes } from 'react-router';
 import ReactRouterLibrary, { ReactRouterPage1, ReactRouterPage2 } from './react-router/ReactRouterLibrary';
+import Layout from './react-router/Layout';
+import AxiosLibrary from './axios/AxiosLibrary';
+import ReactCookieLibrary from './react-cookie/ReactCookieLibrary';
 
 function App() {
   return (
@@ -11,17 +14,20 @@ function App() {
        <Route path='/page1' element={<ReactRouterPage1/>}/>
        <Route path='/page2' element={<ReactRouterPage2/>}/>
        <Route path='/zustand' element={ <ZustandLibraty />}/> */}
-       <Route path='/router'>
+
+       <Route path='/router' element={<Layout/>}>
           <Route index element={<ReactRouterLibrary/>}/>
-          <Route path='page1' element={<ReactRouterPage1/>}/>
-          <Route path='page2' element={<ReactRouterPage2/>}/>
+          <Route path='page1/:value' element={<ReactRouterPage1/>}/>
+          <Route path='page2/' element={<ReactRouterPage2/>}/>
        </Route>
        <Route path='zustand' element={ <ZustandLibraty />}/>
-       {/* <ZustandLibraty/> */}
+       <Route path='/axios' element={<AxiosLibrary/>}/>
+       <Route path='/cookie' element={<ReactCookieLibrary/>}/>
+       <Route path='*' element={<h3>404 Error</h3>} />
     </Routes>
     //<ZustandLibraty/>
   );
 }
 
 export default App;
-// 복습완료!!!
+//!!!복습완료
